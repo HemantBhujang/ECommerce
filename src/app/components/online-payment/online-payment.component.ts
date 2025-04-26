9d16ac49dc24aa3ab36b9a531d960335be66318f
sakshi Kakde <sakshi.kakde2004@gmail.com>
2025-04-26 16:36:31 +0530

    checkout compelete

--- src/app/components/online-payment/online-payment.component.ts
+++ src/app/components/online-payment/online-payment.component.ts
@@ -1,5 +1,5 @@
-// online-payment.component.ts
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CartProduct } from 'src/app/Services/cart.service';
 import { CheckoutService } from 'src/app/Services/checkout.service';
 
@@ -9,54 +9,72 @@ import { CheckoutService } from 'src/app/Services/checkout.service';
   styleUrls: ['./online-payment.component.css']
 })
 export class OnlinePaymentComponent {
-    cartItems: CartProduct[] = [];
-  
-     image=''
-     address='';
-     arrivalDate='';
-     productNames='';
-     orderTotal=''
-     user: any = null;
-     products: any[] = [];
-     getItemTotal=0;
-     getDeliveryTotal=0;
-  
-  
-      constructor(
-        private chechoutService: CheckoutService
-       ) {}
-  
-       ngOnInit(){
-       this.loadOrderSummary()
-       }
-  
-       loadOrderSummary(){
-        this.chechoutService.getOrderSummary().subscribe({
-          next: (data) => {
-            this.user = data;
-            console.log(this.user);
-            
-            this.arrivalDate=this.user.arrivalDate
-            this.address=this.user.address
-          //  this.name=this.user.products.name
-            this.orderTotal = this.user.orderTotal
-          console.log('order total :',this.orderTotal)
-        
-            // const productNames = this.user.products.map((product: any) => product.name);
-            // console.log(productNames);
-            //  this.productNames = productNames;
-  
-            this.products = this.user.products;
-            console.log('Products:', this.products);
-            this.getItemTotal=Number(this.products.length);
-  
-            this.getDeliveryTotal = this.products.reduce((sum, product) => {
-              return sum + (product.delivery || 0); // if delivery is undefined, treat as 0
-            }, 0);
-      
-            console.log('Total Delivery Charge:', this.getDeliveryTotal);
-         
-          }
-        })
-       }
+  cartItems: CartProduct[] = [];
+
+  image = '';
+  address = '';
+  arrivalDate = '';
+  productNames = '';
+  orderTotal = '';
+  user: any = null;
+  products: any[] = [];
+  getItemTotal = 0;
+  getDeliveryTotal = 0;
+  id: number | null = null;
+
+  constructor(
+    private checkoutService: CheckoutService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.id = idParam ? Number(idParam) : null;
+    console.log('ID from URL:', this.id);
+
+    this.loadOrderSummary();
+  }
+
+  loadOrderSummary() {
+    if (this.id) {
+      // If ID is present → load one product order summary
+      this.checkoutService.getOneProductOrderSummary(this.id).subscribe({
+        next: (data) => {
+          console.log('Single Product Order Summary:', data);
+          this.setOrderData(data);
+        },
+        error: (err) => {
+          console.error('Error fetching single product summary', err);
+        }
+      });
+    } else {
+      // If no ID → load normal cart order summary
+      this.checkoutService.getOrderSummary().subscribe({
+        next: (data) => {
+          console.log('Cart Order Summary:', data);
+          this.setOrderData(data);
+        },
+        error: (err) => {
+          console.error('Error fetching cart order summary', err);
+        }
+      });
+    }
+  }
+
+  setOrderData(data: any) {
+    this.user = data;
+    this.arrivalDate = this.user.arrivalDate;
+    this.address = this.user.address;
+    this.orderTotal = this.user.orderTotal;
+    this.products = this.user.products || [];
+    console.log('Products:', this.products);
+
+    this.getItemTotal = this.products.length;
+    this.getDeliveryTotal = this.products.reduce((sum, product) => {
+      return sum + (product.delivery || 0);
+    }, 0);
+
+    console.log('Total Item Count:', this.getItemTotal);
+    console.log('Total Delivery Charge:', this.getDeliveryTotal);
+  }
 }
--- src/app/components/order-confirmed/order-confirmed.component.ts
+++ src/app/components/order-confirmed/order-confirmed.component.ts
@@ -1,10 +1,7 @@
-// order-confirmed.component.ts
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { CartProduct, CartService } from 'src/app/Services/cart.service';
+import { ActivatedRoute } from '@angular/router';
+import { CartProduct } from 'src/app/Services/cart.service';
 import { CheckoutService } from 'src/app/Services/checkout.service';
-import { DatabaseCartService } from 'src/app/Services/database-cart.service';
-import { LoginService } from 'src/app/Services/login.service';
 
 @Component({
   selector: 'app-order-confirmed',
@@ -12,55 +9,73 @@ import { LoginService } from 'src/app/Services/login.service';
   styleUrls: ['./order-confirmed.component.css']
 })
 export class OrderConfirmedComponent {
-   cartItems: CartProduct[] = [];
+  cartItems: CartProduct[] = [];
 
-   image=''
-   address='';
-   arrivalDate='';
-   productNames='';
-   orderTotal=''
-   user: any = null;
-   products: any[] = [];
-   getItemTotal=0;
-   getDeliveryTotal=0;
+  image = '';
+  address = '';
+  arrivalDate = '';
+  productNames = '';
+  orderTotal = '';
+  user: any = null;
+  products: any[] = [];
+  getItemTotal = 0;
+  getDeliveryTotal = 0;
+  id: number | null = null;
 
+  constructor(
+    private checkoutService: CheckoutService,
+    private route: ActivatedRoute
+  ) {}
 
-    constructor(
-      private chechoutService: CheckoutService
-     ) {}
+  ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.id = idParam ? Number(idParam) : null;
+    console.log('ID from URL:', this.id);
 
-     ngOnInit(){
-     this.loadOrderSummary()
-     }
+    this.loadOrderSummary();
+  }
 
-     loadOrderSummary(){
-      this.chechoutService.getOrderSummary().subscribe({
+  loadOrderSummary() {
+    if (this.id) {
+      // If ID is present → load one product order summary
+      this.checkoutService.getOneProductOrderSummary(this.id).subscribe({
         next: (data) => {
-          this.user = data;
-          console.log(this.user);
-          
-          this.arrivalDate=this.user.arrivalDate
-          this.address=this.user.address
-        //  this.name=this.user.products.name
-          this.orderTotal = this.user.orderTotal
-        console.log('order total :',this.orderTotal)
-      
-          // const productNames = this.user.products.map((product: any) => product.name);
-          // console.log(productNames);
-          //  this.productNames = productNames;
-
-          this.products = this.user.products;
-          console.log('Products:', this.products);
-          this.getItemTotal=Number(this.products.length);
-
-          this.getDeliveryTotal = this.products.reduce((sum, product) => {
-            return sum + (product.delivery || 0); // if delivery is undefined, treat as 0
-          }, 0);
-    
-          console.log('Total Delivery Charge:', this.getDeliveryTotal);
-       
+          console.log('Single Product Order Summary (Order Confirmed):', data);
+          this.setOrderData(data);
+        },
+        error: (err) => {
+          console.error('Error fetching single product order summary', err);
         }
-      })
-     }
+      });
+    } else {
+      // If no ID → load normal cart order summary
+      this.checkoutService.getOrderSummary().subscribe({
+        next: (data) => {
+          console.log('Cart Order Summary (Order Confirmed):', data);
+          this.setOrderData(data);
+        },
+        error: (err) => {
+          console.error('Error fetching cart order summary', err);
+        }
+      });
+    }
+  }
 
+  setOrderData(data: any) {
+    this.user = data;
+    this.arrivalDate = this.user.arrivalDate;
+    this.address = this.user.address;
+    this.orderTotal = this.user.orderTotal;
+    this.products = this.user.products || [];
+    console.log('Products:', this.products);
+
+    this.getItemTotal = this.products.length;
+    this.getDeliveryTotal = this.products.reduce((sum, product) => {
+      return sum + (product.delivery || 0);
+    }, 0);
+
+    console.log('Total Item Count:', this.getItemTotal);
+    console.log('Total Delivery Charge:', this.getDeliveryTotal);
+  }
 }
+
